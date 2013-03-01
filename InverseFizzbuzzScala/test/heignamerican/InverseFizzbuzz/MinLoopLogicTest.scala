252af37c7cf89f0320b353e1a2173cd71d9bbf48
Masashi HOSODA <hosoda@kurusugawa.jp>
2013-03-02 00:33:02 +0900

    リファクタリング まとめた

--- InverseFizzbuzzScala/test/heignamerican/InverseFizzbuzz/MinLoopLogicTest.scala
+++ InverseFizzbuzzScala/test/heignamerican/InverseFizzbuzz/MinLoopLogicTest.scala
@@ -12,30 +12,24 @@ class MinLoopLogicTest extends AssertionsForJUnit {
   }
 
   @Test def test() {
-    checkSingle(Seq("Fizz"), Result(3))
-    checkSingle(Seq("Buzz"), Result(5))
-    checkSingle(Seq("FizzBuzz"), Result(15))
-    checkSingle(Seq("FizzBuzz", "Fizz"), Result(15, 18))
-    checkSingle(Seq("Fizz", "Buzz"), Result(9, 10))
-    checkSingle(Seq("Fizz", "Fizz", "Buzz"), Result(6, 10))
-    checkSingle(Seq("Fizz", "Buzz", "Fizz"), Result(3, 6))
+    assertAnswer(1, 100, Seq("Fizz"), Result(3))
+    assertAnswer(1, 100, Seq("Buzz"), Result(5))
+    assertAnswer(1, 100, Seq("FizzBuzz"), Result(15))
+    assertAnswer(1, 100, Seq("FizzBuzz", "Fizz"), Result(15, 18))
+    assertAnswer(1, 100, Seq("Fizz", "Buzz"), Result(9, 10))
+    assertAnswer(1, 100, Seq("Fizz", "Fizz", "Buzz"), Result(6, 10))
+    assertAnswer(1, 100, Seq("Fizz", "Buzz", "Fizz"), Result(3, 6))
+
+    assertAnswer(20, 100, Seq("Fizz"), Result(21))
+    assertAnswer(20, 100, Seq("Buzz"), Result(25))
+    assertAnswer(20, 100, Seq("FizzBuzz"), Result(30))
+    assertAnswer(20, 100, Seq("FizzBuzz", "Fizz"), Result(30, 33))
+    assertAnswer(20, 100, Seq("Fizz", "Buzz"), Result(24, 25))
+    assertAnswer(20, 100, Seq("Fizz", "Fizz", "Buzz"), Result(21, 25))
+    assertAnswer(20, 100, Seq("Fizz", "Buzz", "Fizz"), Result(24, 27))
   }
 
-  def checkSingle(aInput: Seq[String], aExpected: Result) = {
-    assert(aExpected === new MinLoopLogic(Fizzbuzz3and5, 1, 100).inverseFizzbuzz(aInput))
-  }
-
-  @Test def test2() {
-    checkSingle2(Seq("Fizz"), Result(21))
-    checkSingle2(Seq("Buzz"), Result(25))
-    checkSingle2(Seq("FizzBuzz"), Result(30))
-    checkSingle2(Seq("FizzBuzz", "Fizz"), Result(30, 33))
-    checkSingle2(Seq("Fizz", "Buzz"), Result(24, 25))
-    checkSingle2(Seq("Fizz", "Fizz", "Buzz"), Result(21, 25))
-    checkSingle2(Seq("Fizz", "Buzz", "Fizz"), Result(24, 27))
-  }
-
-  def checkSingle2(aInput: Seq[String], aExpected: Result) = {
-    assert(aExpected === new MinLoopLogic(Fizzbuzz3and5, 20, 100).inverseFizzbuzz(aInput))
+  private def assertAnswer(start: Int, end: Int, aInput: Seq[String], aExpected: heignamerican.InverseFizzbuzz.Result): Unit = {
+    assert(aExpected === new MinLoopLogic(Fizzbuzz3and5, start, end).inverseFizzbuzz(aInput))
   }
 }
