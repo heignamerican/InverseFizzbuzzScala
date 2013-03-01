18725e1045e39b5ab1d493588c77d801bea77f6c
Masashi HOSODA <hosoda@kurusugawa.jp>
2013-03-02 00:21:05 +0900

    class じゃなくて object

--- InverseFizzbuzzScala/src/heignamerican/InverseFizzbuzz/Fizzbuzz3and5.scala
+++ InverseFizzbuzzScala/src/heignamerican/InverseFizzbuzz/Fizzbuzz3and5.scala
@@ -1,5 +1,5 @@
 package heignamerican.InverseFizzbuzz
 
-case class Fizzbuzz3and5 extends FizzbuzzRule {
+object Fizzbuzz3and5 extends FizzbuzzRule {
   def getMap() = Seq((3, "Fizz"), (5, "Buzz"))
 }
